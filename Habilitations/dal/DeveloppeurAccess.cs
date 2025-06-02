1cbdbb51b1ebaf12a0c66468ff1945ce10d39b2f
laeeaea <laemaulino@gmail.com>
2025-06-02 16:11:47 +0200

    Update DeveloppeurAccess.cs

--- Habilitations/dal/DeveloppeurAccess.cs
+++ Habilitations/dal/DeveloppeurAccess.cs
@@ -64,17 +64,24 @@ namespace Habilitations.dal
         /// Récupère et retourne les développeurs
         /// </summary>
         /// <returns>liste des développeurs</returns>
-        public List<Developpeur> GetLesDeveloppeurs()
+        /// <param name="idProfil">id du profil à filtrer (null pour aucun filtre)</param>
+        public List<Developpeur> GetLesDeveloppeurs(int? idProfil = null)
         {
             List<Developpeur> lesDeveloppeurs = new List<Developpeur>();
             if (access.Manager != null)
             {
                 string req = "select d.iddeveloppeur as iddeveloppeur, d.nom as nom, d.prenom as prenom, d.tel as tel, d.mail as mail, p.idprofil as idprofil, p.nom as profil ";
                 req += "from developpeur d join profil p on (d.idprofil = p.idprofil) ";
+                Dictionary<string, object> parameters = new Dictionary<string, object>();
+                if (idProfil.HasValue)
+                {
+                    req+= "where d.idprofil = @idprofil ";
+                    parameters.Add("@idprofil", idProfil.Value);
+                }
                 req += "order by nom, prenom;";
                 try
                 {
-                    List<Object[]> records = access.Manager.ReqSelect(req);
+                    List<Object[]> records = access.Manager.ReqSelect(req, parameters);
                     if (records != null)
                     {
                         Log.Debug("DeveloppeurAccess.GetLesDeveloppeurs nb records = {0}", records.Count);
@@ -98,7 +105,6 @@ namespace Habilitations.dal
             }
             return lesDeveloppeurs;
         }
-
         /// <summary>
         /// Demande de suppression d'un développeur
         /// </summary>
